5be396f5a30e87fe252d40bd2b137084e9770db4
Sebastian Opałczyński <opalczynski@gmail.com>
2016-05-29 22:55:35 +0200

    [INITIAL COMMIT]

---
+++ README.md
@@ -0,0 +1,32 @@
+# I wanna play a game
+
+![I wanna play a game][img/iwannaplayagame.jpg]
+
+# INFO
+
+Each one of you will get an email or facebook meessage with your unique ID. This IS is important.
+
+Probably you didn't have so amazing recruitment experience ever :) But I must do this that way, because, there's 
+twenty one of you. So I need to put minimum effort in this recruitment process. I've decided not to reject a single 
+application on the first (CV) level.
+
+At junior level - you can be overwhelmed a little bit with this README and tasks :) But do not worry - Just do, there's 
+not try. And I think that if you're smart guy or lady - you really can resolve all this - including the learing 
+of how to make git pull request in the given time;
+
+# RULES
+
+1. Task can be found in the tasks/tasks.py file.
+2. Using your ID - create a file under solutions python package, with name: `<ID>.py`; If your ID is 
+ `nice_diamond` the file name will be: `nice_diamond.py`
+3. Do not push changes to master branch! Create a branch which is called the same as your ID - and then create a 
+ PR (pull request) to master branch.
+4. Deadline for creating a PR: 02.06.2016 23:59:59 - this is very serious.
+5. You can ask me only! one and only one! question if you want - so be very careful with this.
+ 
+# WHAT NEXT?
+
+1. Until next sunday evening - the this README will be updated with junior dev ranking.
+2. The most 3-4 will be asked to make a call or meeting with me. 
+
+### Enjoy. Happy coding.
---
+++ tasks/tasks.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+
+# Hello! Good luck!
+
+##############
+#   TASK 1   #
+##############
+
+
+class Ticket(object):
+    def __init__(
+        self,
+        ticket_id,    # the unique identifier of the ticket;
+        event_date,   # the date of the event;
+        event_time,   # the time when event occurs;
+        event_name,   # the event name -> obvious one :);
+        client,       # the INSTANCE of the client;
+        room_number,  # the room number in which event happens;
+    ):
+        self.ticket_id = ticket_id
+        self.event_date = event_date
+        self.event_time = event_time
+        self.event_name = event_name
+        self.client = client
+        self.room_number = room_number
+
+# SUBTASK 1
+# Define the Client class with attributes:
+# first_name, last_name, birth_date, sex
+
+# SUBTASK 2
+# define a method on Ticket: get_ticket_data, which returns dumped JSON object
+# with all data on ticket, and all data on client;
+# EXAMPLE:
+#     {
+#         "event_time": "20:00",
+#         "event_name": "The Killer",
+#         "room_number": "Sala 7",
+#         "client":
+#             {
+#                 "birth_date": "1985-01-23",
+#                 "first_name": "Romuald",
+#                 "last_name": "Lechon",
+#                 "sex": "MALE"
+#             },
+#         "ticket_id": "12345678",
+#         "event_date": "2015-09-10"
+#     }
+
+# SUBTASK 3
+# Write a method (name: can_watch_pegi ;)) on client which check if he is able
+# to watch the movie - according to PEGI
+# (https://pl.wikipedia.org/wiki/Pan_European_Game_Information)
+# (yep, I know - PEGI is for games, but who cares) - assume following age
+# marks: 3, 7, 12, 16, 18;
+# Method should return True or False -> True means that client is older than
+# restriction and can watch the movie;
+
+# SUBTASK 4
+# Propose a structure where client instance will have some interface to get the
+# all ticket ids - whenever added, wich returns all ticket ids;
+# EXAMPLE (it's just an example, remember - "some interface" - be creative):
+# c = Client(...)
+# t = Ticket('12345678', client=c, ...)
+# t = Ticket('8765431', client=c, ...)
+# t = Ticket('12343141', client=c, ...)
+# c.get_ticket_ids()
+# OUTPUT: ['12345678', '8765431', '12343141']
+
+
+##############
+#   TASK 2   #
+##############
+
+# SUBTASK 1
+# do it in pythonic way;
+# it just adds the index to the list element on this index
+# and return a new list;
+# TIP: shorter is better;
+
+def iterate_over_list(some_list):
+    i = 0
+    new_list = []
+    for element in some_list:
+        n_element = int(element) + i
+        new_list.append(n_element)
+        i = i + 1
+
+    return new_list
+
+# SUBTASK 2
+# and how handle this?
+# in such case - method should repeat string index times;
+# input -> ['a', 'b', 'c']; output: ['', 'b', 'cc'];
+try:
+    iterate_over_list(['a', 'b', 'c'])
+except ValueError:
+    print('Ups. We have an error here.')
+
+##############
+#   TASK 3   #
+##############
+
+# Imagine that you have a database table, defined as follows:
+# TABLE poi
+#   COLUMN id SERIAL (primary_key)
+#   COLUMN name TEXT
+#   COLUMN location_city TEXT
+#   COLUMN location_country TEXT
+#   COLUMN latitude NUMERIC
+#   COLUMN longitude NUMERIC
+
+# Write an SQL query which returns the aggregated poi by location_city column;
+# We want to know how many poi (Pint of interest is in London)
+# So we want to get, something like this:
+# city|pois_count
+# London|123
+# Warsaw|541
+# Moscow|32
+
+# SUBTASK 1
+# Assume that above query will be done quite frequently, additionally
+# there will be also a query which will be filtering (WHERE) on location_city.
+# What can we do to improve database performance?
