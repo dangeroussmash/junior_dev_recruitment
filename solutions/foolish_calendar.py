323f9aa52a693ed1826da6c96211e9cfdc398dd8
msmolik <mateusz.smolik@vpplant.pl>
2016-06-01 23:34:14 +0200

    Convert examples section to simple asserts. Added note about PEGI.

--- solutions/foolish_calendar.py
+++ solutions/foolish_calendar.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import datetime
 import json
+import random
 
 ##############
 #   TASK 1   #
@@ -58,6 +59,8 @@ class Client(object):
             except ValueError:
                 return "Wrong birth_date format. Expecting YYYY-MM-DD on input."
 
+            # PEGI permission is based only on calendar year value, not full birth value.
+            # For example users with birth date 01-01-1998 and 01-12-1998 have the same right to buy 18+ game.
             if today.year - birth_datetime.year >= pegi:
                 return True
             else:
@@ -69,37 +72,6 @@ class Client(object):
         """Return all associated tickets ids."""
         return [ticket for ticket in self.tickets]
 
-# Examples:
-import random
-
-c = Client("Romuald", "Leon", "1999-06-23", "MALE")
-c1 = Client("Romuald", "Leon", "1999-16-23", "MALE")
-print(c.can_watch_pegi(2))
-print(c.can_watch_pegi(3))
-print(c.can_watch_pegi(18))
-print(c1.can_watch_pegi(3))
-
-
-def ticket_fabric():
-    for x in range(100):
-        Ticket(
-            ticket_id=random.randint(10000, 20000),
-            client=c,
-            event_date='2016-{}-11'.format(
-                random.randint(1, 12)
-            ),
-            event_time='{}:00'.format(
-                random.randint(10, 22)
-            ),
-            event_name='The Killer 2',
-            room_number="Sala {}".format(
-                random.randint(1, 8)
-            )
-        )
-
-ticket_fabric()
-print(c.get_all_tickets_by_ids())
-print(c1.get_all_tickets_by_ids())
 
 ##############
 #   TASK 2   #
@@ -111,9 +83,6 @@ def iterate_over_list(some_list):
         return [int(element) + index for index, element in enumerate(some_list)]
     except ValueError:
         return [element * index for index, element in enumerate(some_list)]
-
-iterate_over_list([1, 2, 3])
-iterate_over_list(['a', 'b', 'c'])
 
 
 ##############
@@ -131,3 +100,39 @@ r"""
 r"""
   CREATE INDEX poi_location_city_idx ON poi (location_city);
 """
+
+
+if __name__ == '__main__':
+    c = Client("Romuald", "Leon", "1999-06-23", "MALE")
+    c1 = Client("Romuald", "Leon", "1999-16-23", "MALE")
+
+    def ticket_fabric():
+        for x in range(100):
+            Ticket(
+                ticket_id=random.randint(10000, 20000),
+                client=c,
+                event_date='2016-{}-11'.format(
+                    random.randint(1, 12)
+                ),
+                event_time='{}:00'.format(
+                    random.randint(10, 22)
+                ),
+                event_name='The Killer 2',
+                room_number="Sala {}".format(
+                    random.randint(1, 8)
+                )
+            )
+
+
+    ticket_fabric()
+
+    assert c1.get_all_tickets_by_ids() == []
+    assert c.can_watch_pegi(2) == 'Wrong PEGI mark!'
+    assert c.can_watch_pegi(3) is True
+    assert c.can_watch_pegi(18) is False
+    assert c1.can_watch_pegi(3) == 'Wrong birth_date format. Expecting YYYY-MM-DD on input.'
+
+    assert iterate_over_list([1, 2, 3]) == [1, 3, 5]
+    assert iterate_over_list([1, 2, 3, 4, 5, 10, 11, 12, 13]) == [1, 3, 5, 7, 9, 15, 17, 19, 21]
+    assert iterate_over_list(['a', 'b', 'c']) == ['', 'b', 'cc']
+    assert iterate_over_list(['a', 'b', 'c', 'd', 'e', 'f']) == ['', 'b', 'cc', 'ddd', 'eeee', 'fffff']
